fd50f0e75165fa9b302f56fa2e985d96c2c37611
koushik481 <koushik.ra23@sahyadri.edu.in>
2026-03-02 14:21:50 +0530

    my first package

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_py_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='koushik',
+    maintainer_email='koushik@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'publisher = my_py_pkg.publisher:main',
+            'subscriber = my_py_pkg.subscriber:main',
+        ],
+    },
+)
